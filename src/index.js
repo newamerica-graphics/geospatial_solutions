2308ee4b13d149e7eab66aff84d6c28fea73f8aa
stephen-darling <darling@newamerica.org>
2025-06-11 10:21:55 -0400

    more bug fixes

--- src/index.js
+++ src/index.js
@@ -1,37 +1,26 @@
-import './index.scss';
+import "./index.scss";
 
 let queue = [];
 let data = null;
 
 const settings = {
-  'viz__map': (el) => {
-        let a = el.querySelector('.dataviz__title-container')
-    let b = el.querySelector('.dataviz__chart-container')
-    a && a.remove()
-    b && b.remove()
+    viz__map: (el) => {
+        let a = el.querySelector(".dataviz__title-container");
+        let b = el.querySelector(".dataviz__chart-container");
+        a && a.remove();
+        b && b.remove();
 
-    el.classList.remove('na-dataviz')
+        el.classList.remove("na-dataviz");
 
-    let target = document.createElement("iframe")
-    target.src = "https://temp-rtl.netlify.app/"
-    el.appendChild(target)
-  }
+        let target = document.createElement("iframe");
+        target.src = "https://temp-rtl.netlify.app/";
+        el.appendChild(target);
+    },
 };
-
-fetch('endpoint').then(response => response.json()).then((_data)=>{
-  data = _data;
-  for(let i=0; i<queue.length; i++)
-    queue[i]();
-});
 
 window.renderDataViz = function(el){
   let id = el.getAttribute('id');
   let chart = settings[id];
   if(!chart) return;
-
-  if(data){
-    chart(el);
-  } else {
-    queue.push(() => chart(el));
-  }
+  chart(el);
 }
